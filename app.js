6feb04263210acfa719cf00b25450ade8e31a932
henriqueEz4568 <rick.scioli@gmail.com>
2023-10-15 22:38:02 -0300

    iniciando o servidor

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 const express=require('express')
+
+
+
+const app=express()
+const porta=3000
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('<h1>minha lista de tarefas</h1>')
+})
+app.listen(porta,()=>{
+    console.log('servidor iniciado na porta'+porta)
+})
