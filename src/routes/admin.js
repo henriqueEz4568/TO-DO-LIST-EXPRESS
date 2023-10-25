23e2847b933832428e1feaff08cc22dcd8530bd3
henriqueEz4568 <rick.scioli@gmail.com>
2023-10-24 22:24:27 -0300

    rotas POST funcionando

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,40 +1,52 @@
-const express=require('express')
-const router=express.Router()
-//app.use(express.static('public'))
-const Checklist=require('../models/checklist')
+const express = require('express');
+const router = express.Router();
+const Checklist = require('../models/checklist');
+
+router.get('/checklist', (req, res) => {
+    res.send();
+});
+
+router.get('/sexo', (req, res) => {
+    res.send('sexo');
+});
+
+router.get('/', (req, res) => {
+    console.log('olá');
+    res.send('pagina checklist');
+});
+
+router.post('/', async(req, res) => {
+   let {name}= req.body;
+try{let checklist=await Checklist.create({ name })  
+res.status(200).json(checklist)}catch(error){res.status(422).json(error)}
+console.log({name})
 
 
+   // res.status(200).json(req.body);
+});
+router.get('/criando:teste',(req,res)=>{
+    const id =req.params.teste;
+    res.send(`pagina teste e id ${id}`)})
+router.post('/criando:teste',async(req,res)=>{let { name } = req.body;
+try{
+    let checklist=await Checklist.create({ name })
+    res.status(200).json(checklist)
+}catch(error){res.status(422).json(error)}})
 
-router.get('/checklist', (req,res)=>{
-    res.send('rota checklist')
-})
 
-router.get('/sexo',(req,res)=>{res.send('sexo')})
+router.get('/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send(`ID: ${req.params.id}`);
+});
 
-router.get('/', (req,res)=>{
-console.log('olá')
-res.send('pagina checklist')})
-
-router.post('/',(req,res)=>{
-console.log(req.body);
-res.status(200).json(req.body)
-})
-
-router.get('/:id',(req,res)=>{console.log(req.params.id);
-res.send(`ID: ${req.params.id}`)})
-
-router.put('/id:',(req,res)=>{
-console.log(req.body);
-res.send(` put ID: ${req.params.id}`)    
-
-})
-
-router.delete('/id:',(req,res)=>{
+router.put('/:id', (req, res) => {
     console.log(req.body);
-    res.send(`DELETE ID: ${req.params.title}`)    
-    
-    })
+    res.send(`PUT ID: ${req.params.id}`);
+});
 
+router.delete('/:id', (req, res) => {
+    console.log(req.body);
+    res.send(`DELETE ID: ${req.params.id}`);
+});
 
-
-module.exports=router
+module.exports = router;
