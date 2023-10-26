8a3e8d7a563f7dcf364167830e73f2ac8563c92f
henriqueEz4568 <rick.scioli@gmail.com>
2023-10-25 22:33:45 -0300

    rotas put e delete funcionando

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -6,9 +6,7 @@ router.get('/checklist', (req, res) => {
     res.send();
 });
 
-router.get('/sexo', (req, res) => {
-    res.send('sexo');
-});
+
 
 router.get('/',async (req, res) => {
     try{
@@ -36,19 +34,40 @@ try{
 }catch(error){res.status(422).json(error)}})
 
 
-router.get('/:id', (req, res) => {
-    console.log(req.params.id);
-    res.send(`ID: ${req.params.id}`);
+router.get('/:id', async(req, res) => {try{
+    let checklist =await Checklist.findById(req.params.id);
+    res.status(200).json(checklist)
+}catch(error){res.status(422).json(error)}
+    
 });
 
-router.put('/:id', (req, res) => {
-    console.log(req.body);
-    res.send(`PUT ID: ${req.params.id}`);
+router.put('/:id',async (req, res) => {
+let {name} = req.body
+try{let checklist=await Checklist.findByIdAndUpdate(req.params.id, {name},{new:true}) 
+res.status(200).json(checklist)
+
+   }catch(error){res.status(422).json(error)
+
+   }
 });
 
-router.delete('/:id', (req, res) => {
-    console.log(req.body);
-    res.send(`DELETE ID: ${req.params.id}`);
+router.delete('/:id', async(req, res) => {
+let {name}=req.body;
+try{let checklist=await Checklist.findByIdAndDelete(req.params.id,{name},{new:true})
+res.status(200).json(checklist) }catch(error){res.status(422).json(error)}   
+    
 });
+
+router.post('/:id',async(req,res)=>{let {name}=req.body;
+let id =req.params.id
+try{
+let checklist=await Checklist.create({name});
+res.status(200).json(checklist)
+}catch(error){
+    res.status(422).json(error)
+}
+
+
+})
 
 module.exports = router;
